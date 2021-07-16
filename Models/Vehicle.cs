12f7388d58aaae4e81aece110d5cbf3d1fc13f99
Ammon Johnson <ammonsawesome@gmail.com>
2021-07-16 15:24:26 -0700

    Created a Veicle class

---
+++ Models/Vehicle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBinding.Models
+{
+    /// <summary>
+    /// Represents a vehicle
+    /// </summary>
+    public class Vehicle
+    {
+        /// <summary>
+        /// The company that manufactured the vehicle
+        /// </summary>
+        public string Make { get; set; }
+
+        /// <summary>
+        /// Name of the vehicle
+        /// </summary>
+        public string Model { get; set; }
+
+        /// <summary>
+        /// The year the vehicle was made
+        /// </summary>
+        public DateTime Year {get; set; }
+
+        /// <summary>
+        /// The price of the vehicle
+        /// </summary>
+        public  double Price { get; set; }
+
+        /// <summary>
+        /// Gasoline or diesel
+        /// </summary>
+        public string FuelType { get; set; }
+
+        /// <summary>
+        /// Truck, car, or motorcycle 
+        /// </summary>
+        public string BodyType { get; set; }
+
+        /// <summary>
+        /// Gas mileage
+        /// </summary>
+        public double GasMileage { get; set; }
+
+    }
+}
